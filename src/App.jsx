0f5f4c0cc8b1b9dc50e2a29f5da44a8e5016d234
GiordanoCasale <giordanocasale8@gmail.com>
2025-05-13 14:46:26 +0200

    {UPD} Milestone 1 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 
 function App() {
+  const [actors, setActors] = useState([]);
+
+  useEffect(() => {
+    fetchActors();
+  }, []);
+
+  const fetchActors = () => {
+    axios.get("https://lanciweb.github.io/demo/api/actors/")
+      .then((response) => {
+        console.log(response.data); // Stampa i dati in console
+        setActors(response.data.result);
+      })
+      .catch((error) => console.error("Errore nel caricamento degli attori:", error));
+  }
 
   return (
     <div>
-      <h1>hello world</h1>
+      <ul className="list-group">
+        {actors.map((actor) => (
+          <li key={actor.id} className="list-group-item">{actor.name}</li>
+        ))}
+      </ul>
     </div>
   )
 }
